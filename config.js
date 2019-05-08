dce329e1b6f4266a9cdee8fe5d00bd8d0c67ba02
max@obby.co.uk <max@obby.co>
2019-05-08 20:26:14 +0100

    Initial commit from gatsby: (https://github.com/maxkufner/blog.git)

---
+++ config.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  url: 'https://maxkufner.co',
+  title: 'MKs blog',
+  subtitle: 'co-founder @ObbyUK, former strategy consultant @cilconsultants, love @fcbayern & 🏌️‍♂️',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Max Kufner',
+    photo: '/photo.jpg',
+    bio: 'co-founder @ObbyUK, former strategy consultant @cilconsultants, love @fcbayern & 🏌️‍♂️',
+    contacts: {
+      email: 'maximiliankufner@gmail.com',
+      twitter: 'maxkufner',
+      github: 'maxkufner',
+      linkedin: 'maxkufner'
+    }
+  }
+};
---
+++ content/pages/about.md
@@ -0,0 +1,11 @@
+---
+template: page
+title: About me
+slug: /pages/about
+draft: false
+---
+co-founder [@ObbyUK](https://obby.co.uk/), former strategy consultant, love @fcbayern &🏌️
+
+This blog was inspired by [Mark Christian](https://writing.markchristian.org/2019/04/29/personal-web-sites/) & [Andrew Chen](https://andrewchen.co/professional-blogging/) to have a place to store all my (currently non-existent) content in one place.
+
+The blog itself runs on [Gatsby](https://www.gatsbyjs.org/) (+ [Lumen](https://github.com/alxshelepenok/gatsby-starter-lumen) theme). Feel free to report any bugs [here](https://github.com/maxkufner/blog).
